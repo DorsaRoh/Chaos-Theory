bcf0126ffb96cec4ef8ebf266750880305c4090c
Dorsa R <dorsa.rohani@gmail.com>
2023-03-20 22:36:12 -0400

    Update logisticmap.py

--- logisticmap.py
+++ logisticmap.py
@@ -1,4 +1,14 @@
-#brief bifurcation calculator
+def plt_show(plt, width, dpi=100):
+    bytes = io.BytesIO()
+    plt.savefig(bytes, format='png', dpi=dpi)
+    if hasattr(plt, "close"):
+        plt.close()
+    bytes.seek(0)
+    base64_string = "data:image/png;base64," + \
+        base64.b64encode(bytes.getvalue()).decode("utf-8")
+    return "<img src='" + base64_string + "' width='" + str(width) + "'>"
+
+# bifurcation calculator
 def main(inputs):
     global inv
     global endv
