e4ded8d0a2a7d05a9b35d1da70a61d6d82d5e2d9
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-08-18 13:28:56 -0400

    add requirements.txt file

--- lorenzattractors.py
+++ lorenzattractors.py
@@ -1,3 +1,22 @@
+import time
+startTime = time.time()
+import numpy as np
+import matplotlib.pyplot as plt
+import base64
+import io
+from scipy.integrate import solve_ivp
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri as mtri
+from scipy.integrate import odeint
+from matplotlib import animation
+from matplotlib.colors import cnames
+from matplotlib import cm
+from scipy import integrate
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import matplotlib as mpl
+
 def plt_show(plt, width, dpi=100):
     bytes = io.BytesIO()
     plt.savefig(bytes, format='png', dpi=dpi)
